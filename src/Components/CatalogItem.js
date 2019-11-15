5130871fe26a6afff4a690d39ed1768bef55c2da
Byron Hill <byron@cautioncreation.com>
2019-11-14 17:43:11 -0700

    Add quick view modal for products

--- src/Components/CatalogItem.js
+++ src/Components/CatalogItem.js
@@ -1,27 +1,49 @@
 import React, { Component } from 'react';
 
-import { Link } from 'react-router-dom';
+import CatalogView from './CatalogView';
 
 import { Col, Card, Button } from 'react-bootstrap';
 
 class CatalogItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = ({
+      buttonClicked: false
+    });
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({buttonClicked: !this.state.buttonClicked});
+  }
+
   render() {
-    let location = "/shop/" + this.props.id;
     let fileName = "./" + this.props.src + ".jpg";
     let images = require.context('../Assets/products');
     let source = images(fileName);
 
     return(
-      <Col className="catalog-item" xs="12" md="6" lg="4">
-        <Card className="catalog-item__container">
-          <Link className="catalog-item__image-container" to={location}>
-            <img className="catalog-item__image" src={source} alt="Product" />
-          </Link>
-          <h3 className="catalog-item__text">${this.props.price}</h3>
-          <h3 className="catalog-item__text">{this.props.name}</h3>
-          <Button className="catalog-item__button" size="block">Buy Now</Button>
-        </Card>
-      </Col>
+      <React.Fragment>
+        <Col className="catalog-item" xs="12" md="6" lg="4">
+          <Card className="catalog-item__container">
+            <button className="catalog-item__image-container" onClick={this.handleClick}>
+              <img className="catalog-item__image" src={source} alt="Product" />
+            </button>
+            <h3 className="catalog-item__text">${this.props.price}</h3>
+            <h3 className="catalog-item__text">{this.props.name}</h3>
+            <Button className="catalog-item__button" size="block" onClick={this.handleClick}>Quick View</Button>
+          </Card>
+        </Col>
+        <CatalogView
+          show={this.state.buttonClicked}
+          handleClick={this.handleClick}
+          name={this.props.name}
+          price={"$" + this.props.price}
+          src={source}
+        />
+    </React.Fragment>
     )
   }
 }
---
+++ src/Components/CatalogView.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import { Modal, Container, Row, Col, Button } from 'react-bootstrap';
+
+class CatalogView extends Component {
+  render() {
+    return (
+      <Modal className="catalog-view" size="lg" aria-labelledby="Catalog-View" show={this.props.show} onHide={this.props.handleClick} centered>
+      <Modal.Header className="catalog-view__header" closeButton />
+      <Modal.Body className="catalog-view__body">
+        <Container className="catalog-view__container" fluid>
+          <Row className="catalog-view__row">
+            <Col className="catalog-view__column" xs="12" md="6">
+              <div className="catalog-view__image-container">
+                <img className="catalog-view__image" src={this.props.src} alt={this.props.name} />
+              </div>
+            </Col>
+            <Col className="catalog-view__column" xs="12" md="6">
+              <div className="catalog-view__text-container">
+                <h3 className="catalog-view__text">{this.props.name}</h3>
+                <hr />
+                <h3 className="catalog-view__text">{this.props.price}</h3>
+              </div>
+              <div className="catalog-view__text-container">
+                <p className="catalog-view__description">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </div>
+              <div className="catalog-view__button-container">
+                <Button className="catalog-view__button" size="block">Add to cart</Button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Modal.Body>
+    </Modal>
+    )
+  }
+}
+
+export default CatalogView;
