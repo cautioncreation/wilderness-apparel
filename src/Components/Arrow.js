ae265fc557f2899478f5f43efa97ed90a53c96f6
byron hill <byron@cautioncreation.com>
2019-11-19 14:52:52 -0700

    fixed JSON and Router

--- src/Components/Arrow.js
+++ src/Components/Arrow.js
@@ -5,10 +5,10 @@ import ArrowImage from '../Assets/svg/Arrow.svg';
 class Arrow extends Component {
   render() {
     return (
-      <a className="arrow" href="#HomeCategories">
+      <div className="arrow">
         <h5 className="arrow__text">Shop Now</h5>
         <img className="arrow__icon" src={ArrowImage} alt="arrow" />
-      </a>
+      </div>
     )
   }
 }
--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, HashRouter as Router } from 'react-router-dom';
 import './scss/Styles.scss'
 
 import Home from './Components/Home';
